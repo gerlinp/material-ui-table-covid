8043909a29bf921e1c263c2ddb610da0e5aabf09
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-03-06 12:42:12 -0500

    data fix

--- src/components/ExamsTable.jsx
+++ src/components/ExamsTable.jsx
@@ -1,44 +1,6 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import MaterialReactTable from 'material-react-table';
 
-
-
-// const ExamsTable = () => {
-//   const [exams, setExams] = useState([])
-
-//   const fetchData = () => {
-//     fetch("https://czi-covid-1-hjgxknco3a-uc.a.run.app/api/exams")
-//     .then(response => {
-//       return response.json()
-//     })
-//     .then(data => {
-//       setExams(data)
-//     })
-//   }
-
-//   useEffect(() => {
-//     fetchData()
-//   }, [])
-
-//   while (length < 0) {
-//     console.log(`empty`)
-//   } 
-
-//   console.log(exams)
-
-//   return (
-//     <div>
-//       {length > 0 && (
-//         <ul>
-//           {map(exam => (
-//             <li key={exam.patientId}>{exam.patientId}</li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   )
-// }
-
 function ExamsTable() {
   const [data, setExamData] = useState([]);
   const url = 'https://czi-covid-1-hjgxknco3a-uc.a.run.app/api/exams';
@@ -47,73 +9,67 @@ function ExamsTable() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
-      setExamData(data);
+      console.log(data.exams);
+      setExamData(data.exams);
     } catch (error) {
       console.error(error);
     }
   }
 
-  // console.log(`Stevens ${jsonArr}`)
-
   useEffect(() => {
     fetchData(url);
   }, []);
-
-
-  // const exams = Array.from(data)
-  // console.log(`Hello from ${exams.exams}`)
 
   const columns = useMemo(
     () => [
       {
         accessorKey: 'patientId',
         header: 'ID',
-        width: 20
+        width: 20,
       },
       {
         accessorKey: 'age',
         header: 'Age',
-        width: 20
+        width: 20,
       },
       {
         accessorKey: 'sex',
         header: 'Sex',
-        width: 20
+        width: 20,
       },
       {
         accessorKey: 'zipCode',
         header: 'Zip Code',
-        width: 20
+        width: 20,
       },
       {
         accessorKey: 'examId',
         header: 'Exam ID',
-        width: 20
+        width: 20,
       },
       {
         accessorKey: 'keyFindings',
         header: 'Key Findings',
-        width: 20
+        width: 20,
       },
       {
         accessorKey: 'brixiaScores',
         header: 'Brixia Scores',
-        width: 20
+        width: 20,
       },
       {
         accessorKey: 'imageURL',
         header: 'Image',
-        width: 20
+        width: 20,
       },
     ],
-    [],
+    []
   );
 
   return (
-    <div style={{ maxWidth: '75%', margin: 'auto'}}>
+    <div style={{ maxWidth: '75%', margin: 'auto' }}>
       <h1>Exam Data</h1>
-      <MaterialReactTable columns={columns} data={JSON.stringify(data)} />
+      <MaterialReactTable columns={columns} data={data} />
     </div>
   );
 }
